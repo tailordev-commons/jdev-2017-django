2ef54e0e926ed2655411c24b8772ffffa5da5b71
Julien Maupetit <julien@tailordev.fr>
2017-06-29 08:29:05 +0200

    Improve records exploration in the admin

--- temperature/admin.py
+++ temperature/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import Country, Record
 
 
+class RecordAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('country', 'date', 'temperature', 'uncertainty')
+    list_filter = ('country', )
+
+
 admin.site.register(Country)
-admin.site.register(Record)
+admin.site.register(Record, RecordAdmin)
